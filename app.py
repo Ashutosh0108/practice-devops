e76fba611cb695c9c4ca941ca63f22c3e94a44e8
Ashutosh Kumar Singh <ashutoshsinghvns08@gmail.com>
2026-02-09 15:02:07 +0530

    Restoring CI/CD workflow files

--- app.py
+++ app.py
@@ -1,97 +1,97 @@
-from flask import Flask, jsonify
-from datetime import datetime
-
-app = Flask(__name__)
-
-@app.route('/health', methods=['GET'])
-def health():
-    return jsonify({
-        "status": "ok",
-        "service": "ecs-demo",
-        "timestamp": datetime.utcnow().isoformat() + "Z"
-    })
-
-if __name__ == '__main__':
-   
-    app.run(host='0.0.0.0', port=5000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from flask import Flask
+# from flask import Flask, jsonify
+# from datetime import datetime
 
 # app = Flask(__name__)
 
-
-# @app.route("/")
-# def home():
-#     return "Welcome to the Flask webapp!"
-
-
-# @app.route("/health")
+# @app.route('/health', methods=['GET'])
 # def health():
-#     return {"status": "ok"}
+#     return jsonify({
+#         "status": "ok",
+#         "service": "ecs-demo",
+#         "timestamp": datetime.utcnow().isoformat() + "Z"
+#     })
+
+# if __name__ == '__main__':
+   
+#     app.run(host='0.0.0.0', port=5000)
 
 
-# @app.route("/greet/<name>")
-# def greet(name):
-#     return f"Hello, {name}!"
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=False)
 
 
-# # testing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask webapp!"
+
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.route("/greet/<name>")
+def greet(name):
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
+
+# testing
